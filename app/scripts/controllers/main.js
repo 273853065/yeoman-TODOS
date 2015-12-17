3c0b5450a5d0b3090a0679f6cc27fdb3b39c5499
lavender <273853065@qq.com>
2015-12-17 17:55:58 +0800

    # mod: 加入localStorageService后显示有问题，继续查错

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,18 +1,22 @@
 'use strict';
 
 angular.module('yeomanTodosApp')
-  .controller('MainCtrl', function ($scope,localStorageService) {
-    $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
-    $scope.todoa = '';
-    $scope.addTodo = function () {
-      if ($scope.todoa === '') {
-        return;
-      } else {
-        $scope.todos.push($scope.todoa);
+.controller('MainCtrl', function ($scope, localStorageService) {
+        var todosInStore = localStorageService.get('todos');
+        $scope.todos = todosInStore && todosInStore.split('\n') || [];
+        $scope.$watch('todos', function () {
+                localStorageService.add('todos', $scope.todos.join('\n'));
+        }, true);
         $scope.todoa = '';
-      }
-    };
-  $scope.removeTodo = function(index){
-    $scope.todos.splice(index,1);
-  };
-  });
+        $scope.addTodo = function () {
+                if ($scope.todoa === '') {
+                        return;
+                } else {
+                        $scope.todos.push($scope.todoa);
+                        $scope.todoa = '';
+                }
+        };
+        $scope.removeTodo = function (index) {
+                $scope.todos.splice(index, 1);
+        };
+});
