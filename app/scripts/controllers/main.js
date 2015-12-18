f41d7e170132b292340527df29da9d551c401a9e
lavender <273853065@qq.com>
2015-12-18 11:16:45 +0800

    # mod: Todos项目完成，之前是因为缓存中有数据，和todos插入数据条件冲突导致显示错误，加入这句就好了localStorageService.clearAll();

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,22 +1,25 @@
 'use strict';
 
 angular.module('yeomanTodosApp')
-.controller('MainCtrl', function ($scope, localStorageService) {
-        var todosInStore = localStorageService.get('todos');
-        $scope.todos = todosInStore && todosInStore.split('\n') || [];
-        $scope.$watch('todos', function () {
-                localStorageService.add('todos', $scope.todos.join('\n'));
-        }, true);
-        $scope.todoa = '';
-        $scope.addTodo = function () {
-                if ($scope.todoa === '') {
-                        return;
-                } else {
-                        $scope.todos.push($scope.todoa);
-                        $scope.todoa = '';
-                }
-        };
-        $scope.removeTodo = function (index) {
-                $scope.todos.splice(index, 1);
-        };
-});
+        .controller('MainCtrl', function ($scope, localStorageService) {
+                //        localStorageService.clearAll();
+                var todosInStore = localStorageService.get('todos');
+                console.log(todosInStore);
+                $scope.todos = todosInStore && todosInStore.split('\n') || [];
+                console.log($scope.todos);
+                $scope.$watch('todos', function () {
+                        localStorageService.add('todos', $scope.todos.join('\n'));
+                }, true);
+                $scope.todoa = '';
+                $scope.addTodo = function () {
+                        if ($scope.todoa === '') {
+                                return;
+                        } else {
+                                $scope.todos.push($scope.todoa);
+                                $scope.todoa = '';
+                        }
+                };
+                $scope.removeTodo = function (index) {
+                        $scope.todos.splice(index, 1);
+                };
+        });
